f60fcc07d8f73fe80393d14a8f2182ca190aa822
IntiDev <yeseniase@outlook.it>
2018-11-08 23:45:36 -0600

    Se agrega versión mobile

---
+++ js/app.js
@@ -0,0 +1,11 @@
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems);
+  });
+
+
+  // $(document).ready(function(){
+  //   $('.sidenav').sidenav();
+  // });
+  //     
